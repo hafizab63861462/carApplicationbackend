7f9105f907e8c346acc653c214e8fa039101a6f3
Hafiz Abdullah <hafiz.abdullahoffical@gmail.com>
2024-07-07 19:18:08 +0500

    car added

--- src/controller/carConntroller.js
+++ src/controller/carConntroller.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/")
+  },
+  filename: function (req, file, cb) {
+    const imageName = Date.now() + file.originalname
+    cb(null, imageName);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 module.exports = (MongoDb) => {
-  router.post('/addCar', async (req, res) => {
+  router.post('/addCar', upload.array("image"), async (req, res) => {
     try {
-      const { car_model, price, phone, city, number_of_pics, uploadimage } = req.body;
+      const { car_model, price, phone, city, number_of_pics } = req.body;
 
+
+      const files = req.files;
+
+      const car = new MongoDb.Car({ car_model, price, phone, city, number_of_pics });
+      await car.save();
 
       res.send({ message: 'car added' });
     } catch (error) {
